76736351ee86cb0bffb6f590f276b893ab1155b0
viveikjha <vivek@aries.res.in>
2020-07-08 18:11:32 +0530

    01_background_estimation.ipynb

--- alignment.py
+++ alignment.py
@@ -10,13 +10,17 @@ filename='*fits'
 
 s=sorted(glob(os.path.join(image_path,filename)))
 print(len(s))
-ref=s[1]
-for i in range(len(s)):
-    view_image(s[i],1) 
-#image=fits.open(ref)
-#reference_image=image[0].data
+ref=s[67]
 '''
-for i in range(50,len(s)):
+for i in range(69,len(s)): #42,43,44,45 do again
+    print(i)
+    view_image(s[i],1)
+'''
+ref=s[69] # image no 3,46,50 and 69 have been used.
+image=fits.open(ref)
+reference_image=image[0].data
+
+for i in range(69,len(s)):
     image_data=fits.open(s[i])
     source_image=image_data[0].data
     header=image_data[0].header
@@ -26,4 +30,3 @@ for i in range(50,len(s)):
     fits.writeto(aligned_file+'_aligned'+'.fits',image_aligned,header,overwrite=True)
 
     print('No. %i done'%i)
-'''
